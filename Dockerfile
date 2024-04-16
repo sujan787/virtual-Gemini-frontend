0a9de6ae74d6899626a885a9d016e1d16d52099d
sujan787 <sujanmoi787@gmail.com>
2024-04-16 16:36:43 +0530

    added new changes

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Stage 1: Build Express API
+FROM node:16-alpine as build
+
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+
+# Serve with Nginx
+FROM nginx:1.23-alpine
+WORKDIR /usr/share/nginx/html
+RUN rm -rf *
+COPY --from=build /app/dist .
+EXPOSE 80
+ENTRYPOINT ["nginx", "-g", "daemon off;"]
--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -20,7 +20,7 @@ export const UI = ({ hidden, ...props }) => {
     <>
       <div className="fixed top-0 left-0 right-0 bottom-0 z-10 flex justify-between p-4 flex-col pointer-events-none">
         <div className="self-start backdrop-blur-md bg-white bg-opacity-50 p-4 rounded-lg">
-          <h1 className="font-black text-xl">My Virtual GF</h1>
+          <h1 className="font-black text-xl">Gemini 2.0</h1>
           <p>Powered By Gemini AI</p>
         </div>
         <div className="w-full flex flex-col items-end justify-center gap-4">
