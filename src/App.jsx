0ed40f0b77f0cd3e292a37cfa41ef2bd7ae6605b
sujan787 <sujanmoi787@gmail.com>
2024-04-18 01:16:46 +0530

    added new changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import { Canvas } from "@react-three/fiber";
 import { Leva } from "leva";
 import { Experience } from "./components/Experience";
 import { UI } from "./components/UI";
+import ParticleContainer from "./components/ParticleContainer";
 
 function App() {
   return (
-    <>
+    <ParticleContainer>
       <Loader />
       <Leva hidden />
       <UI />
       <Canvas shadows camera={{ position: [0, 0, 1], fov: 30 }}>
         <Experience />
       </Canvas>
-    </>
+    </ParticleContainer>
   );
 }
 
---
+++ src/components/ParticleContainer.jsx
@@ -0,0 +1,99 @@
+
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
+
+
+
+const ParticleContainer = ({ children }) => {
+
+    const particlesInit = useCallback(async (engine) => {
+        console.log(engine);
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container) => {
+        await console.log(container);
+    }, []);
+
+    return (<main className="px-4 md:px-32 lg:px-32 relative h-screen">
+        <Particles
+            className=" absolute z-[-1] top-0 bottom-0 right-0 left-0 "
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: false,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: false,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#eeee",
+                    },
+                    links: {
+                        color: "#eeee",
+                        distance: 100,
+                        enable: false,
+                        opacity: 0,
+                        width: 0,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: true,
+                        speed: 2,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 2000,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.45,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 3 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+        {children}
+
+    </main>);
+}
+
+export default ParticleContainer;
